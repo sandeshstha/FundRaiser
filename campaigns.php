d8b02261ab718bee47e65f2197ae83fe5df9fe4b
sandeshstha <sandeshshrestha2018@gmail.com>
2019-08-16 19:54:09 +0545

    update navbar links

--- campaigns.php
+++ campaigns.php
@@ -20,7 +20,7 @@
         </a>
          
         <div class="nav-links">
-            <a href="#">CAMPAIGNS</a>
+            <a href="campaigns.php">CAMPAIGNS</a>
             <a href="#">DONORS</a>
         </div>
 
