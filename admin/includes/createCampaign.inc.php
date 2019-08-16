18c6b8ad0ddf4a73af4e129c2b2546cc5b3540d5
khoms <sundp.kshetri@gmail.com>
2019-08-16 13:56:33 +0545

    Adding Event/Campaigns by admin

--- admin/includes/createCampaign.inc.php
+++ admin/includes/createCampaign.inc.php
@@ -1,17 +1,22 @@
 <?php
-include_once 'sessions.inc.php';
-$campaigncreator = $data['organizer_username'];
+
+$campaigncreator ="Admin";
+
 if(!isset($_POST['submit'])) { //if one try to access createcampaign.inc.php without access
     header("Location: ../createCampaign.php");
     exit();
 } else {
+
     include_once 'dbh.inc.php'; //creating connection to database
+
     $campaignName = $_POST['campaignName'];
     $campaignType = $_POST['campaignType'];
     $campaignDays = $_POST['campaignDays'];
     $campaignAmount = $_POST['campaignAmount'];
     $campaignDescription = $_POST['campaignDescription'];
     $campaignPhone = $_POST['phone'];
+    $campaignApproval = '1';
+
     // error handling
     if(!preg_match("/^[a-zA-Z'. -]+$/", $campaignName)) {    
         header("Location: ../createCampaign.php?campaign=invalidname");
@@ -25,24 +30,28 @@ if(!isset($_POST['submit'])) { //if one try to access createcampaign.inc.php wit
             header("Location: ../createCampaign.php?campaign=camapignNameTaken");
             exit();
         } else {
+
             // for campaign photo part
+
             $filename = $_FILES['campaignPhoto']['name'];
             $tempname = $_FILES['campaignPhoto']['tmp_name'];
             $filename = md5($filename.time());
             $campaignImagePath = "../assets/images/campaignImages/".$filename;
             
             move_uploaded_file($tempname,$campaignImagePath);
+
              // insert the input value into database
-             $sql = "INSERT INTO campaigns(campaign_name, campaign_type, campaign_days, campaign_amount, campaign_description,campaignPhone,campaignImage,`campaignCreator`) VALUES('$campaignName','$campaignType',$campaignDays,$campaignAmount,'$campaignDescription',$campaignPhone,'$campaignImagePath','$campaigncreator');";
+             $sql = "INSERT INTO campaigns(campaign_name, campaign_type, campaign_days, campaign_amount, campaign_description,campaignPhone,campaignImage,`campaignCreator`,`campaignApproval`) VALUES('$campaignName','$campaignType',$campaignDays,$campaignAmount,'$campaignDescription',$campaignPhone,'$campaignImagePath','$campaigncreator','$campaignApproval');";
              $insertSuccess = mysqli_query($conn, $sql);
 // to-do
              // if campaign input data successfully inserted in database then redirect him to success page with link of organizer profile
              if($insertSuccess) {
                 echo "<h1>inserted into database</h1>";
-                header("Location: ../assets/html/organizerCampaignSuccess.html");
+                header("Location: ../includes/successEvent.php");
              } else {
                  echo $conn->error;
              }
         }
     }
+
 }
---
+++ admin/includes/successEvent.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Event Created Successfully</title>
+</head>
+<body>
+	<?php
+	include '../dashboard.html';
+
+	 ?>
+	 <div>
+	 	<center>
+	 	Event has been created successfully!!!<br>
+	 	<a href="../home.php">Click here to return to Homepage</a>
+	 	</center>
+	 </div>
+
+</body>
+</html>
