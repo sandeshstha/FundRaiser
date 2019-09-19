692aa408209a47c66e781e6a9101414a525eb8ac
shurawz <xuchepran9@gmail.com>
2019-09-19 16:53:14 +0545

    FAQ files

---
+++ something.php
@@ -0,0 +1,82 @@
+<?php
+    include_once 'header.php';
+?>
+
+    <link rel="stylesheet" href="assets/css/pp.css">
+</head>
+<body>
+    <!-- navigation section -->
+    <div class="nav-bar">
+        <a href="index.php">
+            <img src="assets/images/logo.png" alt="Fund Raiser logo">
+        </a>
+         
+        <div class="nav-links">
+            <a href="#">CAMPAIGNS</a>
+            <a href="#">DONORS</a>
+        </div>
+
+        <div class="btn-login-signup">
+            <button type="submit" id="btn-login" onclick="window.location.href='login.php'">LOGIN</button>
+            <button type="submit" id="btn-signup" onclick="window.location.href='signup.php'">SIGN UP</button>
+        </div>
+    </div>
+
+    <!-- body part -->
+
+    <div class="banner">
+        <img src="assets/images/banner.png" alt=""><br><br>
+        <span id="banner-quote">
+            "No one has ever become <br> poor by giving."
+        </span>
+    </div>
+
+    <div class="privatepolicy">
+
+        <div class="boxthree">
+            
+            <h2>FAQs</h2>
+
+            <button class="collapsible">What is FundRaiser?</button>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+            <button class="collapsible">How can we donate using this website?</button>
+
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+            <button class="collapsible">How can we create campaign using this website</button>
+
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+</script>
+            <br><br>
+            <h3>If you have any query, you can write below.</h3>
+            <form method="POST" action="includes/prasna.inc.php">
+                <textarea cols="100" rows="5" name="writeQuery" placeholder="Write your query here!"></textarea>
+                <input type="submit" name="submit" value="Submit">
+            </form>
+
+<?php
+    include_once 'footer.php';
+?>
