42f7511dd20d9ec15cd69dddb25dac6e433dbfe2
sandeshstha <sandeshshrestha2018@gmail.com>
2019-09-21 16:02:19 +0545

    complete donors view section

--- donors.php
+++ donors.php
@@ -43,62 +43,39 @@
                 <th width="25%">Donated Amount</th>
                 <th width="25%">campaign</th>
             </tr>
+            <!-- logic to display donors -->
+            <?php 
+                    include 'includes/dbh.inc.php';
+                    $sql = "SELECT * FROM campaigns,donationproof,donors
+                    WHERE donationproof.admin_approval = 1
+                    AND donors.donor_id = donationproof.donor_id 
+                    AND campaigns.campaign_id = donationproof.campaign_id
+                    ORDER BY donate_amount desc;";
 
+                    $result = mysqli_query($conn,$sql);
+                    $resultCheck = mysqli_num_rows($result);            
+                    if($resultCheck > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $counter = 1;
+                            // $donorId = $row['donor_id'];
+                            // $campaignId = $row['campaign_id'];
+                            $donorName = $row['donor_name'];
+                            $donorAddress = $row['donor_address'];
+                            $donatedAmount = $row['donate_amount'];
+                            $campaignName = $row['campaign_name'];
+                ?>
                 <tr>
-                    <?php $counter = 1;?>
-                        <td><?php echo $counter;?></td>
-                    <?php $counter++;?>
-
-                    <?php
-                        include 'includes/dbh.inc.php';
-                        $sql = "SELECT * FROM donationproof,donors 
-                                WHERE donationproof.admin_approval = 1 
-                                AND donors.donor_id = donationproof.donor_id 
-                                ORDER BY donate_amount desc;";
-                                //AND donors.donor_id = 30 //yo logic chaine nai ho ta?
-                        $result = mysqli_query($conn,$sql);
-                        $resultCheck = mysqli_num_rows($result);            
-                        if($resultCheck > 0) {
-                            while($row = mysqli_fetch_assoc($result)) {
-                                $donorId = $row['donor_id'];
-                                echo $donorId;
-                                $donorName = $row['donor_name'];
-                                echo $donorName;
-                                $donorAddress = $row['donor_address'];
-                                echo $donorAddress;
-                                $donatedAmount = $row['donate_amount'];
-                                echo $donatedAmount;
-
-                                $campaignId = $row['campaign_id'];
-                                echo $campaignId;
-                    ?>
-                        <td><?php echo $donorName; ?></td>
-                        <td><?php echo $donorAddress;?></td>
-                        <td><?php echo $donatedAmount;?></td>
-                    <?php 
-                            }
-                        } 
-                    ?>
-
-
-                    <?php
-                        $sql2 = "SELECT campaign_name FROM campaigns,donationproof
-                        WHERE campaigns.campaign_id = donationproof.campaign_id;";
-                        $result2 = mysqli_query($conn, $sql2);
-                        $resultCheck2 = mysqli_num_rows($result2);
-                        if($resultCheck2 > 0) {
-                            while($row2 = mysqli_fetch_assoc($result2)) {
-                                $campaignName = $row2['campaign_name'];
-                                echo "</br>";
-                                echo $campaignName;
-                    ?>
-                        <td><?php echo $campaignName;?></td>
-                    <?php 
-                            }
-                        }
-                    ?>
+                    <td><?php echo $counter++;?></td>
+                    <td><?php echo $donorName; ?></td>
+                    <td><?php echo $donorAddress; ?></td>
+                    <td><?php echo $donatedAmount; ?></td>
+                    <td><?php echo $campaignName; ?></td>                    
                 </tr>
-            
+                <?php
+                        }
+                    }
+                ?>
+
         </table>
     </div>
 
