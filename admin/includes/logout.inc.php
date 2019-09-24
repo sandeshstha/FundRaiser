3eddf4f6a29609aa6b6fe981e480242ea0015f16
sandeshstha <sandeshshrestha2018@gmail.com>
2019-09-24 10:40:26 +0545

    admin logout

--- admin/includes/logout.inc.php
+++ admin/includes/logout.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST['submit'])) {
+if(isset($_GET['logout']) && $_GET['logout'] == 'done') {
     session_start(); //we need to have the session running in order to destroy it
     session_unset();    // unset all the session variables inside the browser
     session_destroy(); //destroy the session
